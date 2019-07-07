e8e93d60dc1c47ec8c7040cd80829a3134864b88
ArianHeight <edmondxue@outlook.com>
2019-07-06 20:49:51 -0700

    Done Basic Interaction

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ Main will definitely call GameEngine to start the game execution
 
 import System.GameEngine;
 
+import java.io.IOException;
+
 public class Main {
     //entry method, args is cmd line arguments
     public static void main(String args[]) {
@@ -17,5 +19,23 @@ public class Main {
         e.startEngine("GameData/game.sav");
 
         System.out.println("Engine finished running, ending program..."); //placeholder line
+
+        //temp lines
+        java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+        String line;
+        try {
+            while (true) {
+                line = br.readLine();
+                if (line != null) {
+                    if (line.equals("end")) {
+                        break; //break out of the while loop
+                    }
+                    System.out.println(line);
+                }
+            }
+        }
+        catch(IOException error) {
+            System.out.println("IO error occurred");
+        }
     }
 }
